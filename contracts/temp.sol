8e4b37f1a6a7305a79e62edc2ac1304657fbd3cb
jonajoy142 <jonajoy142@gmail.com>
2023-02-26 18:11:43 +0530

    first DApp

---
+++ contracts/temp.sol
@@ -0,0 +1,32 @@
+
+
+0x33b28dB6e3F7da5bCc066E558BbAF4F19C5B43F3
+
+[
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_mood",
+				"type": "string"
+			}
+		],
+		"name": "setMood",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getMood",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
